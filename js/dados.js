91d85defeab7816382cda354c34e6fe894bd33f6
Charles <charlesreismagalhaes@outlook.com>
2023-09-28 13:30:27 -0300

    Um sorveteria com um menu online com whatsapp integrado

---
+++ js/dados.js
@@ -0,0 +1,518 @@
+var MENU = {
+    "picole": [
+        {
+            "id": "1",
+            "img": "./img/cardapio/picole/1.jpg",
+            "name": "Picole Oreo",
+            "dsc": "Picole Oreo",
+            "price": 2.99
+        },
+        {
+            "id": "2",
+            "img": "./img/cardapio/picole/2.jpg",
+            "name": "Picole kwi",
+            "dsc": "picole Kwi",
+            "price": 1.59
+        },
+        {
+            "id": "3",
+            "img": "./img/cardapio/picole/3.jpg",
+            "name": "Picole Amor",
+            "dsc": "Picole com muitos chocolates (Boboms trufado e chocolate)",
+            "price": 3.99
+        },
+        {
+            "id": "4",
+            "img": "./img/cardapio/picole/4.jpg",
+            "name": "Amor Simples",
+            "dsc": "Picole de morango",
+            "price": 1.59
+        },
+        {
+            "id": "5",
+            "img": "./img/cardapio/picole/5.jpg",
+            "name": "Picole Uva",
+            "dsc": "Picole Uva",
+            "price": 2.99
+        },
+        {
+            "id": "6",
+            "img": "./img/cardapio/picole/6.jpg",
+            "name": "Picole Trufado",
+            "dsc": "Picole de coco com leite ninho sabor creme",
+            "price": 6.99
+        },
+        {
+            "id": "7",
+            "img": "./img/cardapio/picole/7.jpg",
+            "name": "Patrocinada",
+            "dsc": "Sabor limao com pequenos pedaços de bolachas de recheio limao",
+            "price": 2.00
+        },
+        {
+            "id": "8",
+            "img": "./img/cardapio/picole/8.jpg",
+            "name": "Primeiro Beijo",
+            "dsc": "Picole de sabor limao cobertura de chocolate com creme dentro",
+            "price": 3.13
+        },
+        {
+            "id": "9",
+            "img": "./img/cardapio/picole/9.jpg",
+            "name": "Picole Limonada",
+            "dsc": "Picole Limao",
+            "price": 1.99
+        },
+        {
+            "id": "10",
+            "img": "./img/cardapio/picole/10.jpg",
+            "name": "Picole Negresco",
+            "dsc": "Picole Negresco",
+            "price": 3.99
+        },
+        {
+            "id": "11",
+            "img": "./img/cardapio/picole/11.jpg",
+            "name": "Picole Melancia",
+            "dsc": "Picole Melancia",
+            "price": 2.49
+        },
+        {
+            "id": "12",
+            "img": "./img/cardapio/picole/12.jpg",
+            "name": "Picole Kwi",
+            "dsc": "Picole Kwi",
+            "price": 0.99
+        }
+    ],
+    "sorvetes": [
+        {
+            "id": "13",
+            "img": "./img/cardapio/sorvete/1.jfif",
+            "name": "Biscovete",
+            "dsc": "biscoito e sorvete",
+            "price": 15.5
+        },
+        {
+            "id": "14",
+            "img": "./img/cardapio/sorvete/2.jpg",
+            "name": "Calice do Sorvete",
+            "dsc": "Taça com bolas de coco com chocolate",
+            "price": 20.99
+        },
+        {
+            "id": "15",
+            "img": "./img/cardapio/sorvete/3.jfif",
+            "name": "Melanciavete",
+            "dsc": "Sorvete de sabor melancia com uma fatia da fruta",
+            "price": 14.49
+        },
+        {
+            "id": "16",
+            "img": "./img/cardapio/sorvete/4.jpg",
+            "name": "Chocolotone",
+            "dsc": "Sorvete com chocolate com pequenos pedaços de cookie",
+            "price": 15.99
+        },
+        {
+            "id": "17",
+            "img": "./img/cardapio/sorvete/5.jpg",
+            "name": "Chocorita",
+            "dsc": "Taça de chocolate com cereja creme e dois palito de chocolate",
+            "price": 11
+        },
+        {
+            "id": "18",
+            "img": "./img/cardapio/sorvete/6.jfif",
+            "name": "Amorito",
+            "dsc": "Copo grande com varios incrementos tanto de frutas e doce junto com sorvete",
+            "price": 24
+        },
+        {
+            "id": "19",
+            "img": "./img/cardapio/sorvete/7.jfif",
+            "name": "Abraço Gelado",
+            "dsc": "Uma taça com 4 sabores com incrementos simples",
+            "price": 10
+        },
+        {
+            "id": "20",
+            "img": "./img/cardapio/sorvete/8.jfif",
+            "name": "Sonhos Fantasia",
+            "dsc": "Tijela de muito sabor",
+            "price": 12.99
+        },
+        {
+            "id": "21",
+            "img": "./img/cardapio/sorvete/9.jfif",
+            "name": "Biscoitality",
+            "dsc": "Burger Au Poivre Kit - 4 Pack",
+            "price": 6.49
+        },
+        {
+            "id": "22",
+            "img": "./img/cardapio/sorvete/10.jfif",
+            "name": "Paixona",
+            "dsc": "Taça com sorvete de coco com chocolate em cima",
+            "price": 13.49
+        },
+        {
+            "id": "23",
+            "img": "./img/cardapio/sorvete/11.jfif",
+            "name": "Sonho gelado ",
+            "dsc": "Sorvete de creme com muitos pedadços de oreo",
+            "price": 16.49
+        },
+        {
+            "id": "24",
+            "img": "./img/cardapio/sorvete/12.jfif",
+            "name": "Chocolate Luxúria",
+            "dsc": "Muito chocolate",
+            "price": 14
+        }
+    ],
+    "sobremesas": [
+        {
+            "id": "25",
+            "img": "./img/cardapio/sobremesas/1.jpg",
+            "name": "Cesta Fada",
+            "dsc": "Cesta com chocolates",
+            "price": 39.99
+        },
+        {
+            "id": "26",
+            "img": "./img/cardapio/sobremesas/2.jpg",
+            "name": "Chifre Negros",
+            "dsc": "Cones de chocolate com oreo",
+            "price": 39.95
+        },
+        {
+            "id": "27",
+            "img": "./img/cardapio/sobremesas/3.jpg",
+            "name": "Perdição",
+            "dsc": "Torta deliciosa",
+            "price": 69
+        },
+        {
+            "id": "28",
+            "img": "./img/cardapio/sobremesas/4.jpg",
+            "name": "Cookie Recheado",
+            "dsc": "Cookie com recheio",
+            "price": 10.99
+        },
+        {
+            "id": "29",
+            "img": "./img/cardapio/sobremesas/5.jpg",
+            "name": "Brooklyn Blackout Company",
+            "dsc": "Brooklyn Blackout",
+            "price": 13.59
+        },
+        {
+            "id": "30",
+            "img": "./img/cardapio/sobremesas/6.jpg",
+            "name": "La doçura",
+            "dsc": "Varios Cones de chocolates",
+            "price": 89
+        },
+        {
+            "id": "31",
+            "img": "./img/cardapio/sobremesas/7.jpg",
+            "name": "Doce Encantado",
+            "dsc": "Cones de chocolate",
+            "price": 5.29
+        },
+        {
+            "id": "32",
+            "img": "./img/cardapio/sobremesas/8.jpg",
+            "name": "Cone Oreo",
+            "dsc": "Cone com sabor de oreo",
+            "price": 4.29
+        },
+        {
+            "id": "33",
+            "img": "./img/cardapio/sobremesas/9.jpg",
+            "name": "Taco Doce",
+            "dsc": "Taco com sorvete e chocolate",
+            "price": 7.19
+        },
+        {
+            "id": "34",
+            "img": "./img/cardapio/sobremesas/10.jpg",
+            "name": "Stan's Donuts",
+            "dsc": "Doce de coco",
+            "price": 3.69
+        },
+        {
+            "id": "35",
+            "img": "./img/cardapio/sobremesas/11.jpg",
+            "name": "Casca Magica",
+            "dsc": "Cone com nuttela dentro",
+            "price": 4.69
+        },
+        {
+            "id": "36",
+            "img": "./img/cardapio/sobremesas/12.jpg",
+            "name": "Torres Chocolate",
+            "dsc": "Famous Chocolate",
+            "price": 69.95
+        }
+    ],
+    "bebidas": [
+        {
+            "id": "37",
+            "img": "./img/cardapio/bebidas/hong-kong-boba-tea-kit-for-6.63841de36d8e5edfafa13023fc303285.jpg",
+            "name": "New Territories",
+            "dsc": "Hong Kong Boba Tea Kit for 6",
+            "price": 59
+        },
+        {
+            "id": "38",
+            "img": "./img/cardapio/bebidas/guys-caliente-margaritas-for-12.ca8c6bc06b8f1039549385ffcebc749d.jpg",
+            "name": "Guy Fieri",
+            "dsc": "Guy's Caliente Margaritas for 12",
+            "price": 69
+        },
+        {
+            "id": "39",
+            "img": "./img/cardapio/bebidas/woodford-reserve-mint-julep-syrup.ef523ac7cbae5f4aba6b058207f490d2.jpg",
+            "name": "Woodford Reserve",
+            "dsc": "Woodford Reserve Mint Julep Syrup",
+            "price": 39
+        },
+        {
+            "id": "40",
+            "img": "./img/cardapio/bebidas/new-orleans-hurricane-mix.4613584fc65cb0787024dd24d2a8f4b3.jpg",
+            "name": "Franco's Hurricane Mix",
+            "dsc": "New Orleans Hurricane Mix",
+            "price": 39
+        },
+        {
+            "id": "41",
+            "img": "./img/cardapio/bebidas/margarita-mix.bd48a000d589d3147b14790af3c33fcd.jpg",
+            "name": "Johnny Sanchez",
+            "dsc": "Margarita Mix",
+            "price": 59
+        },
+        {
+            "id": "42",
+            "img": "./img/cardapio/bebidas/woodford-reserve-mint-julep-syrup-2-pack.0ac76063f151988113cbaabd0eaa829f.jpg",
+            "name": "Woodford Reserve",
+            "dsc": "Woodford Reserve Mint Julep Syrup - 2 Pack",
+            "price": 59
+        },
+        {
+            "id": "43",
+            "img": "./img/cardapio/bebidas/unicorn-parade-milkshake-kit-for-2.9052d04c1cf25b29442048bd3e535f21.jpg",
+            "name": "New Territories",
+            "dsc": "Unicorn Parade Milkshake Kit for 8",
+            "price": 109
+        },
+        {
+            "id": "44",
+            "img": "./img/cardapio/bebidas/chickpea-chiller-kit-for-6.4310765c71ba524b5462ea9330d32446.jpg",
+            "name": "The Hummus & Pita Co.",
+            "dsc": "Chickpea Chiller Kit for 6",
+            "price": 89
+        },
+        {
+            "id": "45",
+            "img": "./img/cardapio/bebidas/old-honey-barn-mint-julep-mixer-200ml.e0b131d6d9b69963706b43fd4334ab74.jpg",
+            "name": "Old Honey Barn Mint Julep",
+            "dsc": "Old Honey Barn Mint Julep Mixer - 200ml Flask",
+            "price": 25
+        },
+        {
+            "id": "46",
+            "img": "./img/cardapio/bebidas/kentucky-derby-mint-julep-gift-set.79720eda4e9c8e3fcf9ecb5c79827f2c.jpg",
+            "name": "Woodford Reserve",
+            "dsc": "Kentucky Derby Mint Julep Gift Set",
+            "price": 59.95
+        },
+        {
+            "id": "47",
+            "img": "./img/cardapio/bebidas/weekender-charleston-bloody-mary-mix-bold-and-spicy.c372868c9937e407a299a22001e210e2.jpg",
+            "name": "Charleston Beverage Company",
+            "dsc": "Weekender | Charleston Bloody Mary Mix Bold & Spicy",
+            "price": 39.95
+        },
+        {
+            "id": "48",
+            "img": "./img/cardapio/bebidas/nola-cold-brew-concentrate-bag-in-box.3df6fde8bd83f29235565984ae8ed22b.jpg",
+            "name": "Grady's Cold Brew",
+            "dsc": "NOLA Cold Brew Concentrate Bag-in-Box",
+            "price": 40
+        }
+    ],
+    "geladinho": [
+        {
+            "id": "49",
+            "img": "./img/cardapio/geladinho/12.jpg",
+            "name": "Bombom Fantasia",
+            "dsc": "Geladinho feito com Bomboms",
+            "price": 2.99
+        },
+        {
+            "id": "50",
+            "img": "./img/cardapio/geladinho/11.jpg",
+            "name": "Na Laka",
+            "dsc": "Geladinho com bisoito Laka",
+            "price": 1.49
+        },
+        {
+            "id": "51",
+            "img": "./img/cardapio/geladinho/10.jpg",
+            "name": "Ameixa",
+            "dsc": "Geladinho feito com ameixa e leite ninho ",
+            "price": 4.99
+        },
+        {
+            "id": "52",
+            "img": "./img/cardapio/geladinho/9.jpg",
+            "name": "Sabor Infancia",
+            "dsc": "Geladinho de leite ninho com uva e leite condensado",
+            "price": 0.49
+        },
+        {
+            "id": "53",
+            "img": "./img/cardapio/geladinho/8.jpg",
+            "name": "Biscoitin",
+            "dsc": "Geladinho com bolacha e leite com chocolate",
+            "price": 5.99
+        },
+        {
+            "id": "54",
+            "img": "./img/cardapio/geladinho/7.jpg",
+            "name": "Moranguete",
+            "dsc": "Geladinho de morango com nuttela",
+            "price": 1.99
+        },
+        {
+            "id": "55",
+            "img": "./img/cardapio/geladinho/6.jpg",
+            "name": "Sabor da Paixão",
+            "dsc": "Geladinho de maracuja",
+            "price": 2.49
+        },
+        {
+            "id": "56",
+            "img": "./img/cardapio/geladinho/5.jpg",
+            "name": "XupLeite",
+            "dsc": "Moranguinho de doce de leite",
+            "price": 3.49
+        },
+        {
+            "id": "57",
+            "img": "./img/cardapio/geladinho/4.jpg",
+            "name": "Moranguinho",
+            "dsc": "Geladinho de morango e nuttela",
+            "price": 2.49
+        },
+        {
+            "id": "58",
+            "img": "./img/cardapio/geladinho/3.jpg",
+            "name": "Refresca",
+            "dsc": "Geladinho de limao e pouco de hordela",
+            "price": 0.49
+        },
+        {
+            "id": "59",
+            "img": "./img/cardapio/geladinho/2.jpg",
+            "name": "Milho",
+            "dsc": "Geladinho de milho",
+            "price": 99
+        },
+        {
+            "id": "60",
+            "img": "./img/cardapio/geladinho/1.jpg",
+            "name": "Agua na boca",
+            "dsc": "Geladinho de maracuja",
+            "price": 79
+        }
+    ],
+    "açai": [
+        {
+            "id": "61",
+            "img": "./img/cardapio/açai/1.jpg",
+            "name": "Açai Fruta",
+            "dsc": "Açai Fruta",
+            "price": 15.99
+        },
+        {
+            "id": "62",
+            "img": "./img/cardapio/açai/2.jpg",
+            "name": "Açai Padrão",
+            "dsc": "Açai com leite ninho e morango",
+            "price": 16.49
+        },
+        {
+            "id": "63",
+            "img": "./img/cardapio/açai/3.jpg",
+            "name": "Açai Festa",
+            "dsc": "Açai leite ninho leite condensado e morangos",
+            "price": 13.99
+        },
+        {
+            "id": "64",
+            "img": "./img/cardapio/açai/4.jpg",
+            "name": "Roleta do Sabor",
+            "dsc": "Roleta com varios tipos de açai",
+            "price": 40.00
+        },
+        {
+            "id": "65",
+            "img": "./img/cardapio/açai/5.jpg",
+            "name": "Barca Açaidera",
+            "dsc": "Barda de açai",
+            "price": 31.00
+        },
+        {
+            "id": "66",
+            "img": "./img/cardapio/açai/6.jpg",
+            "name": "Simples e Romantico",
+            "dsc": "Açai Simples porem delicioso",
+            "price": 5.49
+        },
+        {
+            "id": "67",
+            "img": "./img/cardapio/açai/7.jpg",
+            "name": "Giro Comeu",
+            "dsc": "Roleta de açai com frutas",
+            "price": 30.15
+        },
+        {
+            "id": "68",
+            "img": "./img/cardapio/açai/8.jpg",
+            "name": "Kit Amor",
+            "dsc": "Dois açai deliciosos",
+            "price": 15.20
+        },
+        {
+            "id": "69",
+            "img": "./img/cardapio/açai/9.jpg",
+            "name": "Açai Marmita",
+            "dsc": "Marmita de açai",
+            "price": 32
+        },
+        {
+            "id": "70",
+            "img": "./img/cardapio/açai/10.jpg",
+            "name": "Chef Francis",
+            "dsc": "Recipiente cheio de açai",
+            "price": 25.99
+        },
+        {
+            "id": "71",
+            "img": "./img/cardapio/açai/11.jpg",
+            "name": "Nuvem doce",
+            "dsc": "Açai com confete",
+            "price": 22
+        },
+        {
+            "id": "72",
+            "img": "./img/cardapio/açai/12.jpg",
+            "name": "Doçura",
+            "dsc": "Açai com leite condensado",
+            "price": 16.00
+        }
+    ]
+}
